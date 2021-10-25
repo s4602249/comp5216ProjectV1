a6f69f50b61ca170ca350bc177ae54d64c5a8131
miao6158 <miao6158@uni.sydney.edu.au>
2021-10-25 19:52:14 +0800

    comment of DistanceCalculator.java by Mingle Ao

--- app/src/main/java/comp5216/sydney/edu/au/myproject_v1/utils/DistanceCalculator.java
+++ app/src/main/java/comp5216/sydney/edu/au/myproject_v1/utils/DistanceCalculator.java
@@ -2,15 +2,35 @@ package comp5216.sydney.edu.au.myproject_v1.utils;
 
 import com.google.android.gms.maps.model.LatLng;
 
+/**
+ * a class to calculate the distance of two location
+ * @author Mingle Ao
+ * @version 1.0
+ */
 public class DistanceCalculator {
 
     private static double EARTH_RADIUS = 6378137.0;
 
+    /**
+     * convert degrees to radians
+     * @author Mingle Ao
+     * @version 1.0
+     * @param d a double variable of degrees
+     * @return a double variable of radians
+     */
     private double rad(double d){
         return d * Math.PI / 180.0;
     }
 
-        public double GetDistance(LatLng latLng1, LatLng latLng2){
+    /**
+     * calculate the distance of two location
+     * @author Mingle Ao
+     * @version 1.0
+     * @param latLng1 a LatLng variable of location1
+     * @param latLng2 a LatLng variable of location2
+     * @return a double variable of the distance in meter
+     */
+    public double GetDistance(LatLng latLng1, LatLng latLng2){
         double radLat1 = rad(latLng1.latitude);
         double radLat2 = rad(latLng2.latitude);
         double a = radLat1 - radLat2;
